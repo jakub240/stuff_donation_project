9ea7f9979f179f8bb725965ff275aad384398a72
Jakub Werder <jakub.werder@gmail.com>
2021-03-17 22:29:33 +0100

    models_added

--- charitydonation/charity/models.py
+++ charitydonation/charity/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+INST_CHOICES = [
+    ('FN', 'fundacja'),
+    ('OP', 'organizacja pozarządowa'),
+    ('ZL', 'zbiórka lokalna')
+      ]
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    type = models.CharField(max_length=2, choices=INST_CHOICES, default='FN')
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=30)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=30)
+    zip_code = models.IntegerField()
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField(auto_now_add=True)
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
--- charitydonation/charity/views.py
+++ charitydonation/charity/views.py
@@ -4,6 +4,19 @@ from django.views import View
 # Create your views here.
 
 class Landing_Page(View):
+    def get(self, request):        
+        return render(request, 'index.html')
+
+
+class Register(View):
     def get(self, request):
-        return render(request, 'base.html')
-        
+        return render(request, 'register.html')
+
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, 'form.html')
+
+
+
+
